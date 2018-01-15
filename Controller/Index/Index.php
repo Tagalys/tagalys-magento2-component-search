165a13c76e8b61f4c21863ca801d36dcd6c2470a
Palaniappan Chellappan <pc@tagalys.com>
2018-01-15 21:12:55 +0530

    initial commit

---
+++ Controller/Index/Index.php
@@ -0,0 +1,35 @@
+<?php
+ 
+namespace Tagalys\Search\Controller\Index;
+ 
+use Magento\Framework\App\Action\Context;
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\View\Page\Config $pageConfig,
+        \Magento\Framework\App\RequestInterface $request
+    )
+    {
+        $this->_resultPageFactory = $resultPageFactory;
+        $this->pageConfig = $pageConfig;
+        $this->request = $request;
+        parent::__construct($context);
+    }
+ 
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+
+        $params = $this->request->getParams();
+        $resultPage->getConfig()->getTitle()->set('Search Results for "' . $params['q'] . '"');
+
+        $this->pageConfig->setRobots('NOINDEX,NOFOLLOW');
+
+        return $resultPage;
+    }
+}
